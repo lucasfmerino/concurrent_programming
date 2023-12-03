709ddee7597f3c0e11bb3cd8f0cbb221d29beb77
lucasfmerino <lucasfmerino@gmail.com>
2023-12-03 00:29:50 -0300

    feat(Server): ThreadPool added.

--- src/Server/ServerTasks.java
+++ src/Server/ServerTasks.java
@@ -2,6 +2,8 @@ package Server;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ServerTasks {
     public static void main(String[] args) throws Exception {
@@ -9,14 +11,20 @@ public class ServerTasks {
         System.out.println("--- INCIANDO SERVIDOR ---");
         ServerSocket server = new ServerSocket(12345);
 
+        // ExecutorService threadPool = Executors.newFixedThreadPool(2);
+        ExecutorService threadPool = Executors.newCachedThreadPool();
+        
+
         while(true) {
             Socket socket = server.accept();
             System.out.println("Aceitando novo cliente na porta " + socket.getPort());
 
             DistributeTasks distributeTasks = new DistributeTasks(socket);
-            Thread threadClient = new Thread(distributeTasks);
-            threadClient.start();
 
+            // Thread threadClient = new Thread(distributeTasks);
+            // threadClient.start();
+
+            threadPool.execute(distributeTasks);
         }
         
     }
